bfa79adbc78785020067f153f4060645175a2ef5
qkrwpgud1121 <qkrwpgud1121@gmail.com>
2023-08-11 16:58:18 +0900

    Initial Commit

---
+++ Union/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Union
+//
+//  Created by JehyeongPark on 2023/08/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
