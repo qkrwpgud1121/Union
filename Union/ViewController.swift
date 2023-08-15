256c1bc4f43d55042b4ed0fb4ac43e0fb5b290a7
qkrwpgud1121 <qkrwpgud1121@gmail.com>
2023-08-15 14:28:34 +0900

    임시 비밀번호 발급 및 회원가입 UI 생성

--- Union/ViewController.swift
+++ Union/ViewController.swift
@@ -3,17 +3,28 @@
 //  Union
 //
 //  Created by JehyeongPark on 2023/08/11.
-//  박제형(git test)
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func btn_login(_ sender: Any) {
+        
+    }
 
+    @IBAction func temp(_ sender: Any) {
+        
+    }
+    
+    @IBAction func register(_ sender: Any) {
+        
+    }
+    
 }
 
