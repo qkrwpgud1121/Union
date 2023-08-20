32a29226588ac46ac560af76720dd9171a03a244
qkrwpgud1121 <qkrwpgud1121@gmail.com>
2023-08-21 00:52:36 +0900

    app icon, lunchScreen 추가 및 메인 화면 배너 추가

---
+++ Union/BannerCell.swift
@@ -0,0 +1,13 @@
+//
+//  BannerCell.swift
+//  Union
+//
+//  Created by JehyeongPark on 2023/08/19.
+//
+
+import UIKit
+
+class BannerCell: UICollectionViewCell {
+    
+    @IBOutlet var imgView: UIImageView!
+}
--- Union/MainList.swift
+++ Union/MainList.swift
@@ -1,5 +1,5 @@
 //
-//  MainListViewController.swift
+//  MainList.swift
 //  Union
 //
 //  Created by JehyeongPark on 2023/08/19.
@@ -8,16 +8,73 @@
 import UIKit
 
 class MainList: UIViewController {
+    
+    @IBOutlet var bannerCollectionView: UICollectionView!
+    
+    // 현재페이지 체크 변수 (자동 스크롤할 때 필요)
+    var nowPage: Int = 0
 
+    // 데이터 배열
+    let dataArray: Array<UIImage> = [UIImage(named: "img1.png")!, UIImage(named: "img2.png")!, UIImage(named: "img3.png")!]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationController?.interactivePopGestureRecognizer?.isEnabled = false
+        
+        bannerCollectionView.delegate = self
+        bannerCollectionView.dataSource = self
+        
+        bannerTimer()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.navigationItem.hidesBackButton = true
     }
+    
+    // 2초마다 실행되는 타이머
+    func bannerTimer() {
+        let _: Timer = Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { (Timer) in
+            self.bannerMove()
+        }
+    }
+    // 배너 움직이는 매서드
+    func bannerMove() {
+        // 현재페이지가 마지막 페이지일 경우
+        if nowPage == dataArray.count-1 {
+        // 맨 처음 페이지로 돌아감
+            bannerCollectionView.scrollToItem(at: NSIndexPath(item: 0, section: 0) as IndexPath, at: .right, animated: true)
+            nowPage = 0
+            return
+        }
+        // 다음 페이지로 전환
+        nowPage += 1
+        bannerCollectionView.scrollToItem(at: NSIndexPath(item: nowPage, section: 0) as IndexPath, at: .right, animated: true)
+    }
+}
 
+extension MainList: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    //컬렉션뷰 개수 설정
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    //컬렉션뷰 셀 설정
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = bannerCollectionView.dequeueReusableCell(withReuseIdentifier: "BannerCell", for: indexPath) as! BannerCell
+        cell.imgView.image = dataArray[indexPath.row]
+        return cell
+    }
+    
+    // UICollectionViewDelegateFlowLayout 상속
+    //컬렉션뷰 사이즈 설정
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: bannerCollectionView.frame.size.width  , height:  bannerCollectionView.frame.height)
+    }
+    
+    //컬렉션뷰 감속 끝났을 때 현재 페이지 체크
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        nowPage = Int(scrollView.contentOffset.x) / Int(scrollView.frame.width)
+    }
 }
