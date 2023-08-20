d57d9841a8a6ec7e9c4a8c44ec63b54391e464a4
qkrwpgud1121 <qkrwpgud1121@gmail.com>
2023-08-21 01:45:20 +0900

    side Menu 추가

---
+++ Union/CustomSideMenuNavigation.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSideMenuNavigation.swift
+//  Union
+//
+//  Created by JehyeongPark on 2023/08/21.
+//
+
+import Foundation
+import UIKit
+import SideMenu
+
+class CustomSideMenuNavigation: SideMenuNavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ Union/SideMenuViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuViewController.swift
+//  Union
+//
+//  Created by JehyeongPark on 2023/08/21.
+//
+
+import UIKit
+
+class SideMenuViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
