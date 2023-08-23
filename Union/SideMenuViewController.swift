5d7f187e42d7006b32bd06ed082e0d4d482d6d95
qkrwpgud1121 <qkrwpgud1121@gmail.com>
2023-08-23 10:32:01 +0900

    Side menu 추가

--- Union/SideMenuViewController.swift
+++ Union/SideMenuViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class SideMenuViewController: UIViewController {
 
+    @IBOutlet var profile: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        profile.layer.cornerRadius = profile.frame.width / 2
+        profile.layer.borderWidth = 2
+        profile.layer.borderColor = UIColor.gray.cgColor
+        profile.clipsToBounds = true
     }
 
 }
